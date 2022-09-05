5fabf62ca159801a240ba10f05fe48b462d3e1dd
SaraChaLo <112277449+SaraChaLo@users.noreply.github.com>
2022-09-05 23:02:43 +0100

    add the search engine

--- src/app.js
+++ src/app.js
@@ -40,7 +40,20 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
-let apiKey = "1fdeb6e5797d4d955268ac4f9b68ed6f";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "1fdeb6e5797d4d955268ac4f9b68ed6f";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
